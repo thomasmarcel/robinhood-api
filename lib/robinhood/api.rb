e4b33f9472067a04e5bfeb4e841ed25a38c6d2ab
Thomas Alcala Schneider <thomas.alcala@gmail.com>
2018-03-31 23:51:33 -0600

    Linting part of the API

--- lib/robinhood/api.rb
+++ lib/robinhood/api.rb
@@ -1,16 +1,18 @@
 require 'date'
 
 module Robinhood
+  # The API module check for methods that require login.
   module ApiModule
     # Your code goes here...
     def self.before(*names)
       names.each do |name|
         m = instance_method(name)
-        define_method(name) do |*args, &block|  
+        define_method(name) do |*args, &block|
           if token_compliant?(name)
-            m.bind(self).(*args, &block)
+            m.bind(self).call(*args, &block)
           else
-            puts "You have to run the login(<username>, <password>) method or a Robinhood.new instance before running the #{name} method"
+            puts 'You have to run the login(<username>, <password>) method ' \
+              "or a Robinhood.new instance before running the #{name} method"
             exit 1
           end
         end
@@ -18,6 +20,7 @@ module Robinhood
     end
   end
 
+  # The API module instance methods
   module ApiModule
     require 'httparty'
     require 'json'
@@ -26,15 +29,14 @@ module Robinhood
 
     attr_accessor :errors
 
-    def initialize
-    end
+    def initialize; end
 
     def login(username, password)
       raw_response = HTTParty.post(
         endpoints[:login],
         body: {
           'password' => password,
-          'username'=> username
+          'username' => username
         },
         headers: headers
       )
@@ -43,15 +45,17 @@ module Robinhood
         response = response['token']
         @headers['Authorization'] = "Token #{response}"
       end
-      return response
+      response
     end
 
     def investment_profile
-      raw_response = HTTParty.get(endpoints[:investment_profile], headers: headers)
+      raw_response = HTTParty.get(
+        endpoints[:investment_profile], headers: headers
+      )
       JSON.parse(raw_response.body)
     end
 
-    def orders(order_id=nil)
+    def orders(order_id = nil)
       url = order_id ? "#{endpoints[:orders]}#{order_id}" : endpoints[:orders]
       raw_response = HTTParty.get(url, headers: headers)
       JSON.parse(raw_response.body)
@@ -63,22 +67,34 @@ module Robinhood
     end
 
     def portfolio(account_number)
-      raw_response = HTTParty.get("https://api.robinhood.com/accounts/#{account_number}/portfolio/", headers: headers)
+      raw_response = HTTParty.get(
+        "https://api.robinhood.com/accounts/#{account_number}/portfolio/",
+        headers: headers
+      )
       JSON.parse(raw_response.body)
     end
 
     def instruments(symbol)
       if symbol.include?('-')
-        raw_response = HTTParty.get("#{endpoints[:instruments]}#{symbol}/", headers: headers)
+        raw_response = HTTParty.get(
+          "#{endpoints[:instruments]}#{symbol}/", headers: headers
+        )
       else
-        raw_response = HTTParty.get(endpoints[:instruments], query: {'query' => symbol.upcase}, headers: headers)
+        raw_response = HTTParty.get(
+          endpoints[:instruments],
+          query: { 'query' => symbol.upcase },
+          headers: headers
+        )
       end
 
       JSON.parse(raw_response.body)
     end
 
     def quote(symbol)
-      raw_response = HTTParty.get("https://api.robinhood.com/quotes/#{symbol}/", headers: headers)
+      raw_response = HTTParty.get(
+        "https://api.robinhood.com/quotes/#{symbol}/",
+        headers: headers
+      )
       JSON.parse(raw_response.body)
     end
 
@@ -87,10 +103,11 @@ module Robinhood
         endpoints[:orders],
         body: {
           'account' => "https://api.robinhood.com/accounts/#{account_number}/",
-          'instrument' => "https://api.robinhood.com/instruments/#{instrument_id}/",
+          'instrument' => 'https://api.robinhood.com/instruments/' \
+          "#{instrument_id}/",
           'price' => price,
           'quantity' => quantity,
-          'side' => "buy",
+          'side' => 'buy',
           'symbol' => symbol,
           'time_in_force' => 'gfd',
           'trigger' => 'immediate',
@@ -107,10 +124,11 @@ module Robinhood
         endpoints[:orders],
         body: {
           'account' => "https://api.robinhood.com/accounts/#{account_number}/",
-          'instrument' => "https://api.robinhood.com/instruments/#{instrument_id}/",
+          'instrument' => 'https://api.robinhood.com/instruments/' \
+          "#{instrument_id}/",
           'price' => price,
           'quantity' => quantity,
-          'side' => "buy",
+          'side' => 'buy',
           'symbol' => symbol,
           'time_in_force' => 'gfd',
           'trigger' => 'immediate',
@@ -127,10 +145,11 @@ module Robinhood
         endpoints[:orders],
         body: {
           'account' => "https://api.robinhood.com/accounts/#{account_number}/",
-          'instrument' => "https://api.robinhood.com/instruments/#{instrument_id}/",
+          'instrument' => 'https://api.robinhood.com/instruments/' \
+          "#{instrument_id}/",
           'price' => price,
           'quantity' => quantity,
-          'side' => "sell",
+          'side' => 'sell',
           'symbol' => symbol,
           'time_in_force' => 'gfd',
           'trigger' => 'immediate',
@@ -147,10 +166,11 @@ module Robinhood
         endpoints[:orders],
         body: {
           'account' => "https://api.robinhood.com/accounts/#{account_number}/",
-          'instrument' => "https://api.robinhood.com/instruments/#{instrument_id}/",
+          'instrument' => 'https://api.robinhood.com/instruments/' \
+          "#{instrument_id}/",
           'price' => price,
           'quantity' => quantity,
-          'side' => "sell",
+          'side' => 'sell',
           'symbol' => symbol,
           'time_in_force' => 'gfd',
           'trigger' => 'immediate',
@@ -167,10 +187,11 @@ module Robinhood
         endpoints[:orders],
         body: {
           'account' => "https://api.robinhood.com/accounts/#{account_number}/",
-          'instrument' => "https://api.robinhood.com/instruments/#{instrument_id}/",
+          'instrument' => 'https://api.robinhood.com/instruments/' \
+          "#{instrument_id}/",
           'stop_price' => price,
           'quantity' => quantity,
-          'side' => "sell",
+          'side' => 'sell',
           'symbol' => symbol,
           'time_in_force' => 'gtc',
           'trigger' => 'stop',
@@ -183,15 +204,15 @@ module Robinhood
     end
 
     def cancel_order(order_id)
-      raw_response = HTTParty.post("https://api.robinhood.com/orders/#{order_id}/cancel/", headers: headers)
+      raw_response = HTTParty.post(
+        "https://api.robinhood.com/orders/#{order_id}/cancel/", headers: headers
+      )
       raw_response.code == 200
     end
 
     def positions(account_number, instrument_id = nil)
       url = "https://api.robinhood.com/accounts/#{account_number}/positions"
-      if instrument_id
-        url = "#{url}/#{instrument_id}/"
-      end
+      url = "#{url}/#{instrument_id}/" if instrument_id
       raw_response = HTTParty.get(url, headers: headers)
       JSON.parse(raw_response.body)
     end
@@ -223,113 +244,105 @@ module Robinhood
     end
 
     def methodlist
-      [:investment_profile, :accounts, :ach_iav_auth, :ach_relationships, :ach_transfers, :applications, :dividends, :edocuments, :margin_upgrade, :notifications, :orders, :password_reset, :document_requests, :user, :watchlists]
+      %i[investment_profile accounts ach_iav_auth ach_relationships
+         ach_transfers applications dividends edocuments margin_upgrade
+         notifications orders password_reset document_requests user watchlists]
     end
 
     def headers
       @headers ||= {
-        'Accept' => 'application/json',
+        'Accept' => 'application/json'
       }
     end
 
     def token_compliant?(method)
-
       if methodlist.include?(method)
         if headers.key?('Authorization')
-          return true
+          true
         else
-          return false
+          false
         end
       else
-        return true
+        true
       end
     end
 
-    before(*instance_methods) { puts "start" }
+    before(*instance_methods) { puts 'start' }
   end
 
+  # Robinhood API's class methods
   class Api
     include ApiModule
 
-    def self.flatten(h)
-      new_h = {}
-      h.each do |k, v|
+    def self.flatten(var)
+      new_var = {}
+      var.each do |k, v|
         if v.class == Hash
           v.each do |l, w|
-            new_h[l] = w
+            new_var[l] = w
           end
         else
-          new_h[k] = v
+          new_var[k] = v
         end
       end
-      new_h
+      new_var
     end
 
-    def self.sanitize(h)
-      if h.keys.include?('updated_at')
-        h.delete('updated_at')
-      end
+    def self.sanitize(var)
+      var.delete('updated_at') if var.keys.include?('updated_at')
 
-      if h.keys.include?('created_at')
-        h.delete('created_at')
-      end
+      var.delete('created_at') if var.keys.include?('created_at')
 
-      new_h = {}
-      h.each do |k, v|
+      new_var = {}
+      var.each do |k, v|
         key = k
-        if key == 'type'
-          key = 'object_type'
-        end
-        if key == 'id'
-          key = 'third_party_id'
-        end
+        key = 'object_type' if key == 'type'
+        key = 'third_party_id' if key == 'id'
         if v.class == String
-          if v.include?('T') and v.include?('Z')
+          if v.include?('T') && v.include?('Z')
             begin
-              new_h[key] = DateTime.parse(v)
-            rescue
-              new_h[key] = v
+              new_var[key] = Time.parse(v)
+            rescue StandardError
+              new_var[key] = v
             end
           else
             begin
-              new_h[key] = Float(v)
-            rescue
-              new_h[key] = v
+              new_var[key] = Float(v)
+            rescue StandardError
+              new_var[key] = v
             end
           end
         end
       end
-      new_h
+      new_var
     end
 
-    def self.strip(s)
-      return s.split('/').last
+    def self.strip(str)
+      str.split('/').last
     end
 
-    def self.fix(h)
-      if h.class == Hash
-        return self.fix_hash(h)
-      elsif h.class == Array
+    def self.fix(var)
+      if var.class == Hash
+        fix_hash(var)
+      elsif var.class == Array
         arr = []
-        h.each do |ha|
-          arr << self.fix_hash(ha)
+        var.each do |ha|
+          arr << fix_hash(ha)
         end
-        return arr
-      else
-        return nil
+        arr
       end
     end
 
-    def self.fix_hash(h)
-      instance = h.clone
-      instance = self.flatten(instance)
-      instance = self.sanitize(instance)
-      keys = ['account', 'instrument', 'position', 'quote', 'market', 'fundamentals']
+    def self.fix_hash(var)
+      instance = var.clone
+      instance = flatten(instance)
+      instance = sanitize(instance)
+      keys = %w[account instrument position quote market fundamentals]
       keys.each do |key|
-        instance[key] = self.strip(instance[key]) if instance.key?(key)
+        instance[key] = strip(instance[key]) if instance.key?(key)
       end
 
-      return instance
+      instance
     end
   end
 end
--- test/robinhood/api_test.rb
+++ test/robinhood/api_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Robinhood::Api::Test < ActiveSupport::TestCase
-  test "truth" do
+  test 'truth' do
     assert_kind_of Module, Robinhood::Api
   end
 end
